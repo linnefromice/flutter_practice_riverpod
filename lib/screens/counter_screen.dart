c86ed7a0ce77fa2a4e08183ba2949d0864184999
linnefromice <linnefromice@gmail.com>
2021-01-02 21:33:14 +0900

    add state & use provider

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+
+final counterProvider = StateNotifierProvider((ref) => CounterState());
+
+class CounterState extends StateNotifier<int> {
+  CounterState(): super(0);
+
+  void increment() => state++;
+  void decrement() => state--;
+}
+
 class CounterScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, T Function<T>(ProviderBase<Object, T> provider) watch) {
+    final count = watch(counterProvider.state);
+    final counterState = watch(counterProvider);
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -14,16 +27,20 @@ class CounterScreen extends ConsumerWidget {
                 textAlign: TextAlign.center,
             ),
             Text(
-                "0",
+                count.toString(),
                 textAlign: TextAlign.center,
             ),
             ElevatedButton.icon(
-              onPressed: () {},
+              onPressed: () {
+                counterState.increment();
+              },
               icon: Icon(Icons.add),
               label: Text("Plus One"),
             ),
             ElevatedButton.icon(
-              onPressed: () {},
+              onPressed: () {
+                counterState.decrement();
+              },
               icon: Icon(Icons.remove),
               label: Text("Minus One"),
             )
