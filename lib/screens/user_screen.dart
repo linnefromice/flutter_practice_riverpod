9e07e34b1cf4de034870c6b8ac00797ff9b0c1ff
linnefromice <linnefromice@gmail.com>
2021-01-03 00:34:58 +0900

    create state/provider & use state in screen

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_riverpod/all.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:linnefromice/models/user.dart';
 
+final usersProvider = StateNotifierProvider((_) => UsersState());
+
 // ref: http://www.ffotaku.com/final-fantasy-ix/characters.php
-final List<User> users = [
-  User(id: 1, name: "Zidane Tribal", age: 16),
-  User(id: 2, name: "Garnet Til Alexandros XVII", age: 9),
-  User(id: 3, name: "Vivi Ornitier", age: 16),
-  User(id: 4, name: "Adelbert Steiner", age: 33),
-];
+class UsersState extends StateNotifier<List<User>> {
+  UsersState(): super([
+    User(id: 1, name: "Zidane Tribal", age: 16),
+    User(id: 2, name: "Garnet Til Alexandros XVII", age: 9),
+    User(id: 3, name: "Vivi Ornitier", age: 16),
+    User(id: 4, name: "Adelbert Steiner", age: 33),
+  ]);
+}
 
 class UserScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    final state = useProvider(usersProvider.state);
+
     return Scaffold(
       appBar: AppBar(),
       body: Column(
-        children: List.generate(users.length, (index) {
-          final user = users[index].toJson();
+        children: List.generate(state.length, (index) {
+          final user = state[index].toJson();
           return ListTile(
             leading: Text(user["id"].toString()),
             title: Text(user["name"]),
