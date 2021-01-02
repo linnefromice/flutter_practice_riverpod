cba5f1a616fbd5d78f290a8110060283bfd0b6a7
linnefromice <linnefromice@gmail.com>
2021-01-02 17:23:38 +0900

    update ui for counter

--- lib/screens/counter_screen.dart
+++ lib/screens/counter_screen.dart
@@ -6,7 +6,29 @@ class CounterScreen extends ConsumerWidget {
   Widget build(BuildContext context, T Function<T>(ProviderBase<Object, T> provider) watch) {
     return Scaffold(
       body: Center(
-        child: Text("Counter"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+                "Current Counter Number:",
+                textAlign: TextAlign.center,
+            ),
+            Text(
+                "0",
+                textAlign: TextAlign.center,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.add),
+              label: Text("Plus One"),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: Icon(Icons.remove),
+              label: Text("Minus One"),
+            )
+          ],
+        ),
       ),
     );
   }
